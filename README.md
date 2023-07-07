b39f96475823dd39ef5c142b1babfbf7f1c82a1e
hamonangann <bornyto.hamonangan@ui.ac.id>
2023-07-07 18:40:03 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# cloudgirlsimp
+
+Web scraper for downloading [The Cloud Girl](https://thecloudgirl.dev/sketchnote.html) updated sketches. 
+Useful for learning Google Cloud Platform.
+
+Thank you [@pvergadia](https://twitter.com/pvergadia) for providing such a great learning content!
+
+### Prerequisite
+
+Golang and internet connection
+
+### How to run
+```shell
+git clone https://github.com/hamonangann/cloudgirlsimp
+cd cloudgirlsimp
+go mod tidy
+go run main.go
+```
+
+Then go to `/result` to see the updated sketches!
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocolly/colly"
+	"log"
+	"strings"
+)
+
+const ResultDir = "./result/"
+
+func main() {
+	c := colly.NewCollector()
+
+	c.OnHTML(`img[class="article-image"]`, func(e *colly.HTMLElement) {
+		link := e.Attr("src")
+		e.Request.Visit(link)
+	})
+
+	c.OnError(func(r *colly.Response, e error) {
+		log.Println("error:", e, r.Request.URL, string(r.Body))
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		if strings.Index(r.Headers.Get("Content-Type"), "image") > -1 {
+			fmt.Println("downloading image:", r.FileName())
+			r.Save(ResultDir + r.FileName())
+			return
+		}
+	})
+
+	c.Visit("https://thecloudgirl.dev/sketchnote.html")
+}
